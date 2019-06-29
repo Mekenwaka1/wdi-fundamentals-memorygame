6bea3c84b9dc349c00724c82cc047a71326715e3
Osemeke Nwaka <o.ch.nwaka@gmail.com>
2019-06-29 09:39:59 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,18 @@
 /* console.log("User flipped " + cardOne);
 console.log("User flipped " + cardFour); remove? */
+
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+
+function checkForMatch() {
+	if(cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+}
+
+function flipCard(cardId) {
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -15,3 +20,9 @@ if (cardsInPlay.length === 2) {
 		alert("Sorry, try again.");
 	}
 }
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
+flipCard(0);
+flipCard(2);
